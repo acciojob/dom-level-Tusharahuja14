5bfa61e915f35923c1cd6ff6bd47366e1ffb408e
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-25 19:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 let currelement=document.getElementById("level")
-telement=currelement
+telement=currelement;
+let dom=0;
 while(telement)
 	{
 		dom++;
-		telement=telement.parent;
+		telement=telement.parentElement;
 	}
 alert(`The level of the element is: ${dom}`);
